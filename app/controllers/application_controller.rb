ffadd9caef2f8119cfaa203fd092dd5e3f550753
Arundhati <arundhatiraicar12@gmail.com>
2022-04-09 03:16:43 +0530

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,47 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/' do
+    "Hello World"
+  end
+
+  get '/users' do
+    users = User.all 
+    users.to_json
+  end
+
+   get '/reviews' do
+  reviews =Review.all
+   reviews.to_json
+  end
+
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  post '/users' do
+    user=User.create(
+      image: params[:image],
+      name: params[:name],
+      about: params[:about]
+    )
+    user.to_json
+  end
+
+  post '/reviews' do
+    review=Review.create(
+      image: params[:image],
+      product: params[:product],
+      link: params[:link],
+      price: params[:price],
+      size: params[:size],
+      styleNote: params[:styleNote],
+      user_id: params[:user_id]
+      
+    )
+   review.to_json
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_06_131349) do
+ActiveRecord::Schema.define(version: 2022_04_08_213439) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "image"
